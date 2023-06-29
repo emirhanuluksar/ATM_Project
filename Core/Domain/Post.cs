f23ba7f42ffd921dcc370f3ca3faf38c2ab92174
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-06-29 18:10:25 +0300

    changes

--- Core/Domain/Post.cs
+++ Core/Domain/Post.cs
@@ -4,6 +4,6 @@ public class Post : IEntity {
     public int PostId { get; set; }
     public string PostTitle { get; set; } = string.Empty;
     public string PostDescription { get; set; } = string.Empty;
-    public string ProductImageUrl { get; set; } = string.Empty;
-    public DateTime ProductCreationTime { get; set; }
+    public string PostImageUrl { get; set; } = string.Empty;
+    public DateTime PostCreationTime { get; set; }
 }
