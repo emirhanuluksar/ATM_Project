a00a686a57bff3663a66e46420cfc2a4606cbd29
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-07-05 23:07:09 +0300

    DbSet created

--- code/backend/Infrastructure/Persistence/Context/UluarsWebContext.cs
+++ code/backend/Infrastructure/Persistence/Context/UluarsWebContext.cs
@@ -6,8 +6,12 @@ namespace Persistence.Context;
 
 public class UluarsWebContext : DbContext {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        string connectionString = "Server=localhost;Port=5432;Database=Uluarsite;User Id=postgres;Password=100220;Integrated Security=true;";
+        const string connectionString = "Server=localhost;Port=5432;Database=Uluarsite;User Id=postgres;Password=100220;Integrated Security=true;";
         optionsBuilder.UseNpgsql(connectionString);
     }
+
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Post>? Posts { get; set; }
 
 }
